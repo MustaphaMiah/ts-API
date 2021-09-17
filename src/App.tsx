232d9ec018e458d08e1999a057b6c7292cc02268
Mustapha <muhammad_mustapha_miah@live.co.uk>
2021-09-17 17:39:12 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IPost {
+  id: number;
+  userId?: number;
+  title: string;
+  body: string;
 }
 
+const defaultPosts:IPost[] = [];
+
+const App = () => {
+
+const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] = React.useState(defaultPosts);
+const [loading, setLoading]: [boolean, (loading: boolean) => void] = React.useState<boolean>(true);
+const [error, setError]: [string, (error: string) => void] = React.useState("");
+
+React.useEffect(() => {
+  axios
+      .get<IPost[]>("https://jsonplaceholder.typicode.com/posts") 
+        .then(response => {
+          console.log(response.data);
+          setPosts(response.data);
+          setLoading(false);
+        });
+    }, []);
+
+  return (
+    <div className="App">
+     <ul className="posts">
+       {posts.map((post) => (
+        <li key={post.id}>
+          <h2>{post.id}</h2>
+         <h3>{post.title}</h3>
+         <p>{post.body}</p>
+        </li>
+      ))}
+     </ul>
+     {error && <p className="error">{error}</p>}
+   </div>
+   );
+}
 export default App;
